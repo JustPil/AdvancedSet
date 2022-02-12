c7d814beb4da08cc72ce7cf7bfe7707d5fcd0c8d
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-12 11:51:43 -0800

    Added support for generics and copy constructors

--- src/AS/AdvancedSetInterface.java
+++ src/AS/AdvancedSetInterface.java
@@ -1,17 +1,19 @@
 package AS;
 
-public interface AdvancedSetInterface
+public interface AdvancedSetInterface<T>
 {
-    boolean add(int element);
-    boolean contains(int element);
-    boolean remove(int element);
+    boolean add(T element);
+    boolean contains(T element);
+    boolean remove(T element);
     boolean isFull();
     boolean isEmpty();
     int size();
-    int grab();
+    T grab();
     void clear();
     String toString();
-    AdvancedSetInterface union(AdvancedSetInterface set);
-    AdvancedSetInterface intersection(AdvancedSetInterface set);
-    AdvancedSetInterface complement(AdvancedSetInterface set);
+    T[] getArrayCopy();
+    Node<T> getLLCopy();
+    AdvancedSetInterface<T> union(AdvancedSetInterface<T> set);
+    AdvancedSetInterface<T> intersection(AdvancedSetInterface<T> set);
+    AdvancedSetInterface<T> complement(AdvancedSetInterface<T> set);
 }
