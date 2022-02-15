5cfa357e315097ae1142c6e952330efd300c5372
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-15 07:31:04 -0800

    Minor style updates

--- src/AS/AdvancedSetSortedArray.java
+++ src/AS/AdvancedSetSortedArray.java
@@ -3,8 +3,7 @@ package AS;
 import java.util.Comparator;
 import java.util.Random;
 
-public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
-{
+public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T> {
     private int totalItems = 0;
     private final int CAPACITY = 50;
     private T[] array = (T[])new Object[CAPACITY];
@@ -17,10 +16,9 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
     /**
      * Constructor instantiates a Comparator object.
      */
-    public AdvancedSetSortedArray()
-    {
-        comp = new Comparator<T>()
-        {
+    public AdvancedSetSortedArray() {
+        comp = new Comparator<T>() {
+            @Override
             public int compare(T o1, T o2)
             {
                 return ((Comparable)o1).compareTo(o2);
@@ -29,31 +27,24 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
     }
 
     /**
-     * binarySeach A binary search algorithm that finds the index of an element if contained in the array, or the
-     * index that the element should be inserted if not present.
-     * @param element The element to search in the array.
+     * binarySearch A binary search algorithm that finds the index of an element if contained in the Set's internal 
+     * array, or the index that the element should be inserted if not present.
+     * @param element The element to search in the Set's internal array.
      */
-    private void binarySearch(T element)
-    {
+    private void binarySearch(T element) {
         finder = false;
         int start = 0;
         int end = totalItems - 1;
         foundPosition = -1;
-        while(!finder && start <= end)
-        {
+        while(!finder && start <= end) {
             int mid = start + (end - start) / 2;
-            if(comp.compare(array[mid], element) == 0)
-            {
+            if(comp.compare(array[mid], element) == 0) {
                 finder = true;
                 foundPosition = mid;
                 return;
-            }
-            else if(comp.compare(array[mid], element) < 0)
-            {
+            } else if(comp.compare(array[mid], element) < 0) {
                 end = mid - 1;
-            }
-            else
-            {
+            } else {
                 start = mid + 1;
             }
         }
@@ -61,29 +52,21 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
     }
 
     /**
-     * add Adds an element to the array if the array is not full and the element is a unique value.
-     * @param element The element to add into the array.
+     * add Adds an element to the Set's internal array if the array is not full and the element is a unique value.
+     * @param element The element to add to the Set.
      * @return True if the item is added, false if not.
      */
-    public boolean add(T element)
-    {
-        if(totalItems == 0)
-        {
+    public boolean add(T element) {
+        if(totalItems == 0) {
             array[0] = element;
-        }
-        else if(isFull())
-        {
+        } else if(isFull()) {
             return false;
-        }
-        else
-        {
+        } else {
             binarySearch(element);
-            if(finder)
-            {
+            if(finder) {
                 return false;
-            }
-            for(int i = totalItems - 1; i >= insertPosition; i--)
-            {
+            } 
+            for(int i = totalItems - 1; i >= insertPosition; i--) {
                 array[i + 1] = array[i];
             }
             array[insertPosition] = element;
@@ -93,44 +76,33 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
     }
 
     /**
-     * contains Searches the array for an element.
-     * @param element The element to search for in the array.
+     * contains Searches the Set's internal array for an element.
+     * @param element The element to search.
      * @return True if the element is found, false otherwise.
      */
-    public boolean contains(T element)
-    {
+    public boolean contains(T element) {
         binarySearch(element);
         return finder;
     }
 
     /**
-     * remove Removes a specified element from the array.
+     * remove Removes a specified element from the Set's internal array.
      * @param element The element to remove.
      * @return True if the element is removed, false otherwise.
      */
-    public boolean remove(T element)
-    {
+    public boolean remove(T element) {
         binarySearch(element);
-        if(!finder)
-        {
+        if(!finder) {
             return false;
-        }
-        else
-        {
+        } else {
             boolean removeOnce = false;
             T[] newArray = (T[])new Object[CAPACITY];
-            for(int i = 0; i < totalItems; i++)
-            {
-                if(comp.compare(array[i], element) == 0 && !removeOnce)
-                {
+            for(int i = 0; i < totalItems; i++) {
+                if(comp.compare(array[i], element) == 0 && !removeOnce) {
                     removeOnce = true;
-                }
-                else if(!removeOnce)
-                {
+                } else if(!removeOnce) {
                     newArray[i] = array[i];
-                }
-                else
-                {
+                } else {
                     newArray[i - 1] = array[i];
                 }
             }
@@ -141,59 +113,52 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
     }
 
     /**
-     * isFull Determines if the array is full.
-     * @return True if the array is full, false otherwise.
+     * isFull Determines if the Set is full.
+     * @return True if the Set is full, false otherwise.
      */
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return totalItems == CAPACITY;
     }
 
     /**
-     * isEmpty Determines if the array is empty.
-     * @return True if the array is empty, false otherwise.
+     * isEmpty Determines if the Set is empty.
+     * @return True if the Set is empty, false otherwise.
      */
-    public boolean isEmpty()
-    {
+    public boolean isEmpty() {
         return totalItems == 0;
     }
 
     /**
-     * size Determines the number of elements in the array.
-     * @return The number of elements in the array.
+     * size Determines the number of elements in the Set.
+     * @return The number of elements in the Set.
      */
-    public int size()
-    {
+    public int size() {
         return totalItems;
     }
 
     /**
-     * grab Grabs a random element from the array.
+     * grab Grabs a random element from the Set.
      * @return The randomly chosen element.
      */
-    public T grab()
-    {
+    public T grab() {
         return totalItems == 0 ? null : array[random.nextInt(totalItems)];
     }
 
     /**
-     * clear Clears the array of all elements.
+     * clear Clears the Set of all elements.
      */
-    public void clear()
-    {
+    public void clear() {
         array = (T[])new Object[CAPACITY];
         totalItems = 0;
     }
 
     /**
-     * toString Outputs contents of the array in a String.
-     * @return A String containing the output of the array elements.
+     * toString Outputs contents of the Set elements in a String.
+     * @return A String containing the output of the Set elements.
      */
-    public String toString()
-    {
+    public String toString() {
         StringBuilder str = new StringBuilder("[");
-        for(int i = 0; i < totalItems; i++)
-        {
+        for(int i = 0; i < totalItems; i++) {
             str.append(array[i]);
             str.append(i != totalItems - 1 ? " " : "");
         }
@@ -204,8 +169,7 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
      * getArrayCopy A copy constructor providing access to a copy of the internal elements array.
      * @return A copy of the internal elements array.
      */
-    public T[] getArrayCopy()
-    {
+    public T[] getArrayCopy() {
         return array.clone();
     }
 
@@ -213,44 +177,37 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
      * getLLCopy A copy constructor providing access to a copy of the internal elements linked list.
      * @return A copy of the internal elements linked list.
      */
-    public Node<T> getLLCopy()
-    {
+    public Node<T> getLLCopy() {
         return null;
     }
 
     /**
-     * union The union method creates a set that contains all unique elements in the current set and parameter set.
-     * @param set The second set.
-     * @return The union set.
+     * union The union method creates a Set that contains all unique elements in the current Set and parameter Set.
+     * @param set The second Set.
+     * @return The union Set.
      */
-    public AdvancedSetInterface<T> union(AdvancedSetInterface<T> set)
-    {
+    public AdvancedSetInterface<T> union(AdvancedSetInterface<T> set) {
         AdvancedSetInterface<T> union = new AdvancedSetSortedArray<>();
-        for(int i = 0; i < this.size(); i++)
-        {
+        for(int i = 0; i < this.size(); i++) {
             union.add(array[i]);
         }
-        for(int i = 0; i < set.size(); i++)
-        {
+        for(int i = 0; i < set.size(); i++) {
             union.add(set.getArrayCopy()[i]);
         }
         return union;
     }
 
     /**
-     * intersection The intersection method creates a set that contains all elements common to the current set and
-     * the parameter set.
-     * @param set The second set.
-     * @return The intersection set.
+     * intersection The intersection method creates a Set that contains all elements common to the current Set and
+     * the parameter Set.
+     * @param set The second Set.
+     * @return The intersection Set.
      */
-    public AdvancedSetInterface<T> intersection(AdvancedSetInterface<T> set)
-    {
+    public AdvancedSetInterface<T> intersection(AdvancedSetInterface<T> set) {
         AdvancedSetInterface<T> intersection = new AdvancedSetSortedArray<>();
         int minLength = Math.min(this.size(), set.size());
-        for(int i = 0; i < minLength; i++)
-        {
-            if(set.contains(array[i]))
-            {
+        for(int i = 0; i < minLength; i++) {
+            if(set.contains(array[i])) {
                 intersection.add(array[i]);
             }
         }
@@ -258,19 +215,16 @@ public class AdvancedSetSortedArray<T> implements AdvancedSetInterface<T>
     }
 
     /**
-     * complement The complement method creates a set that contains all elements in the current set but not in the
-     * parameter set.
-     * @param set The second set.
-     * @return The relative complement set.
+     * complement The complement method creates a Set that contains all elements in the current Set but not in the
+     * parameter Set.
+     * @param set The second Set.
+     * @return The relative complement Set.
      */
-    public AdvancedSetInterface<T> complement(AdvancedSetInterface<T> set)
-    {
+    public AdvancedSetInterface<T> complement(AdvancedSetInterface<T> set) {
         AdvancedSetInterface<T> complement = new AdvancedSetSortedArray<>();
         int minLength = Math.min(this.size(), set.size());
-        for(int i = 0; i < minLength; i++)
-        {
-            if(!set.contains(array[i]))
-            {
+        for(int i = 0; i < minLength; i++) {
+            if(!set.contains(array[i])) {
                 complement.add(array[i]);
             }
         }
