2393f8ddde85aa7ab31265ea82e95eafa9212744
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 22:12:47 -0800

    Minor style changes

--- src/AS/AdvancedSetInterface.java
+++ src/AS/AdvancedSetInterface.java
@@ -1,7 +1,6 @@
 package AS;
 
-public interface AdvancedSetInterface<T>
-{
+public interface AdvancedSetInterface<T> {
     boolean add(T element);
     boolean contains(T element);
     boolean remove(T element);
